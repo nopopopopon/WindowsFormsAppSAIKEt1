22189921199a6ed4f60b005eb8ebbced08737643
SAIKE <SAIKE@SORA2020>
2021-05-09 05:56:31 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// using Microsoft.VisualBasic.ApplicationServices;  // add .NET5を入れて　Microsoft.VisualBasic.Forms.dllを参照しないと出ない
+
+namespace WindowsFormsAppSAIKEt1
+{
+    /*
+    // 多重起動のときに呼ばれるやつ定義
+    class myApplication : Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase
+    {
+        public myApplication()  : base()
+        {
+            this.EnableVisualStyles = true;
+            this.IsSingleInstance = true;
+            this.MainForm = new Form1();//スタートアップフォームを設定
+            this.StartupNextInstance += new StartupNextInstanceEventHandler(myApplication_StartupNextInstance);
+        }
+        void myApplication_StartupNextInstance(object sender, StartupNextInstanceEventArgs e)
+        {
+            //ここに二重起動されたときの処理を書く
+            //e.CommandLineでコマンドライン引数を取得出来る
+            MessageBox.Show("{0}を開くことができません");
+        }
+    }
+    */
+
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.AllowDrop = true;  // Drop許可に必要
+
+            // ファイルがダブルクリックされたとき
+            string[] files = System.Environment.GetCommandLineArgs();
+            var files1 = files.Skip(1);
+            foreach (var filePath in files1)
+            {
+                if (System.IO.File.Exists(filePath))
+                {
+                    try
+                    {
+                        pictureBox1.ImageLocation = filePath;
+
+                        break;
+                    }
+                    catch
+                    {
+                        MessageBox.Show(String.Format("{0}をダブルクリックで開くことができません", filePath), "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+
+        }
+
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop);
+            
+            if (System.IO.File.Exists(fileName[0]) == true)
+            {
+                pictureBox1.ImageLocation = fileName[0];
+                // Form1.ActiveForm.Text = fileName[0];
+            }
+            else
+            {
+                // ファイルがないなら何もしない
+                MessageBox.Show("ファイルがない");            }
+
+            this.Show();
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;　　//　dragされてきたのがファイルなら受ける
+            else
+                e.Effect = DragDropEffects.None;
+
+        }
+    }
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppSAIKEt1
+{
+    static class Program
+    {
+        // このアプリケーションのエントリーポイントはここ
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // SAIKE追記ここから
+
+            //myApplication winAppBase = new myApplication();
+            //winAppBase.Run(args);
+            if (0 < args.Length)
+            {
+                MessageBox.Show(args[0] + "でし。");
+            }
+            //Mutex名
+            string mutexName = "SAIKEApplicationSUJI";
+            //Mutexオブジェクトを作成する
+            System.Threading.Mutex mutex = new System.Threading.Mutex(false, mutexName);
+
+            bool hasHandle = false;
+            try 
+            { 
+                try
+                {
+                    //ミューテックスの所有権を要求する
+                    hasHandle = mutex.WaitOne(0, false);
+                }
+            //.NET Framework 2.0以降の場合
+                catch (System.Threading.AbandonedMutexException)
+                {
+                    //別のアプリケーションがミューテックスを解放しないで終了した時
+                    hasHandle = true;
+                }
+                
+                //ミューテックスを得られたか調べる
+                if (hasHandle == false)
+                {
+                    //得られなかった場合は、すでに起動していると判断
+                    string  files1 ="" ;
+                    string[] files = System.Environment.GetCommandLineArgs();
+                    if (0 < files.Length)
+                    {
+                        files1 = files[0];
+                    }
+                    //string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop);
+                   
+                    MessageBox.Show(files1 + "多重起動はできません。");
+                    return;
+                }
+
+                //はじめからMainメソッドにあったコードを実行
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+             }
+            finally
+            {
+                if (hasHandle)
+                {
+                    //ミューテックスを解放する
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
+            }
+            
+
+            // SAIKE追記ここまで
+
+            /*
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);  
+            Application.EnableVisualStyles();　　// XP以降のスタイルにする
+            Application.SetCompatibleTextRenderingDefault(false);  // GDIで描画する指定(デフォルト)
+            Application.Run(new Form1());
+            */
+        }
+    }
+}
